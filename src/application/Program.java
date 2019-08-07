953b8aaddd9584e72041fe5a62b8d2104e3888c7
Bruno Farias <bruno43ist@yahoo.com.br>
2019-08-07 20:55:03 -0300

    ex2

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,8 +4,18 @@ public class Program {
 
 	public static void main(String[] args) {
 	
+		//solution of equation xˆ2+x-6=0
+		int a = 1;
+		int b = 1;
+		int c = -6;
 		
-
+		//delta
+		int delta = (b * b) - (4 * a * c);
+		
+		double x1 = ((-b) + Math.sqrt(delta)) / ((double)2*a);
+		double x2 = ((-b) - Math.sqrt(delta)) / ((double)2*a);
+		
+		System.out.println("x1 = " + String.format("%.2f", x1) + "\nx2 = " + String.format("%.2f", x2));
 	}
 
 }
