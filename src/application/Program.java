91f6e49df903cb67cf69db816bd28b44ee79f673
Bruno Farias <bruno43ist@yahoo.com.br>
2019-08-07 21:35:22 -0300

    ex9

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,17 +6,25 @@ public class Program {
 
 	public static void main(String[] args) {
 	
-		/*build a program that reads a number and 
-		prints the multiplication table of this number from 1 to 10*/
+		/*build a program that reads multiple numbers while different -1 and 
+		prints the sum of all these numbers*/
 		
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("Type the number: ");
+		System.out.println("Type a number: ");
 		int a = sc.nextInt();
 		
-		for(int i = 1; i <= 10; i++) {
-			System.out.println(a + " X " + i + " = " + a*i);
+		int sum = 0;
+		
+		if(a != -1) {
+			while(a != -1) {
+				sum += a;
+				System.out.println("Type another number: ");
+				a = sc.nextInt();
+			}
 		}
+		
+		System.out.println("The sum is: " + sum);
 		
 		sc.close();
 	}
