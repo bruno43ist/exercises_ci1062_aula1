f0b84c18bb55b02d5c6fce64bab723a02bebf074
Bruno Farias <bruno43ist@yahoo.com.br>
2019-08-07 21:44:44 -0300

    ex10

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,30 +1,41 @@
 package application;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Program {
 
 	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
 	
-		/*build a program that reads multiple numbers while different -1 and 
-		prints the sum of all these numbers*/
+		/*build a program that reads multiple weights and heights and 
+		returns how many are above ideal weight*/
 		
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("Type a number: ");
-		int a = sc.nextInt();
+		System.out.println("Type your weight: ");
+		double weight = sc.nextDouble();
 		
-		int sum = 0;
+		System.out.println("Type your height: ");
+		double height = sc.nextDouble();
 		
-		if(a != -1) {
-			while(a != -1) {
-				sum += a;
-				System.out.println("Type another number: ");
-				a = sc.nextInt();
+		int counter = 0;
+		
+		while(weight != -1.0 && height != -1.0) {
+			if(weight/(height*height) > 25) {
+				counter += 1;
+			}
+			
+			System.out.println("Type your weight: ");
+			weight = sc.nextDouble();
+			
+			if(weight != -1.0) {
+				System.out.println("Type your height: ");
+				height = sc.nextDouble();
 			}
 		}
 		
-		System.out.println("The sum is: " + sum);
+		System.out.println(counter + " people are above weight! ");
 		
 		sc.close();
 	}
