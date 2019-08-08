64230cff1ca006dc5122b1a9253a6394591707e5
Bruno Farias <bruno43ist@yahoo.com.br>
2019-08-07 21:00:12 -0300

    ex3

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,21 +1,30 @@
 package application;
 
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
 	
-		//solution of equation xˆ2+x-6=0
-		int a = 1;
-		int b = 1;
-		int c = -6;
+		//build a program that shows your entire name, 
+		//your soccer team and your neighborhood
 		
-		//delta
-		int delta = (b * b) - (4 * a * c);
+		Scanner sc = new Scanner(System.in);
 		
-		double x1 = ((-b) + Math.sqrt(delta)) / ((double)2*a);
-		double x2 = ((-b) - Math.sqrt(delta)) / ((double)2*a);
+		System.out.println("Your name: ");
+		String name = sc.nextLine();
 		
-		System.out.println("x1 = " + String.format("%.2f", x1) + "\nx2 = " + String.format("%.2f", x2));
+		System.out.println("Your soccer team: ");
+		String team = sc.nextLine();
+		
+		System.out.println("Your neighborhood: ");
+		String nei = sc.nextLine();
+		
+		System.out.println("Your name is " + name + 
+				"\nYour soccer team is " + team +
+				"\nYour neighborhood is " + nei);
+		
+		sc.close();
 	}
 
 }
