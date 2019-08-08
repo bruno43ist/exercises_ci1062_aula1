552cc4e8df486e860c9a514dace43f1e134f2bd7
Bruno Farias <bruno43ist@yahoo.com.br>
2019-08-07 21:08:27 -0300

    ex6

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,21 +6,28 @@ public class Program {
 
 	public static void main(String[] args) {
 	
-		//build a program that reads a number and 
-		//tells if its positive 
+		/*build a program that reads a number and 
+		returns f(x)
+		0<=x<5, f(x) = x 
+		5<=x<10; f(x) = 2x+1 
+		x>=10; f(x) = x-3 */
 		
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.println("Type a integer number: ");
 		int number = sc.nextInt();
 		
-		if((number % 2) == 0) {
-			System.out.println("This number is even! ");
-		} else {
-			System.out.println("This number is odd! ");
-		}
-		
+		System.out.println("The function returns: " + function(number));
 		sc.close();
+	}
+	
+	public static int function(int x) {
+		if(x >= 0 && x < 5) {
+			return x;
+		} else if(x >= 5 && x < 10) {
+			return (2*x+1);
+		}
+		return (x-3);
 	}
 
 }
